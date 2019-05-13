e0f16d9cc848b99deeceb5aa20da34594dd1d8d1
MelanieL <melanieleeking@gmail.com>
2019-05-13 14:38:19 -0400

    Add files from tutorial part 3, creating nested layouts

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,17 @@
 import React from "react"
 import Header from "../components/header"
+import Layout from "../components/layout"
 
 export default () => (
-    <div style={{ color: `teal` }} >
-        <Header headerText="About Gatsby"/>
-        <Header headerText="It's pretty cool"/>
+  <div style={{ color: `teal` }}>
+    <Layout>
+        <Header headerText="About Gatsby" />
+        <Header headerText="It's pretty cool" />
         <p>Such wow. Very React.</p>
-    </div>
+        <h1>About me</h1>
+        <p>
+            I’m good enough, I’m smart enough, and gosh darn it, people like me!
+        </p>
+    </Layout>
+  </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,20 @@
 import React from "react"
 import { Link } from "gatsby"
 import Header from "../components/header"
+import Layout from "../components/layout"
 
 export default () => (
-    <div style={{ color: `purple` }}>
-        <Link to="/contact/">Contact</Link>
+  <div style={{ color: `purple`, margin: `3rem auto`, maxWidth: 600 }}>
+    <Layout>
         <Header headerText="Hello Gatsby!" />
         <p>What a world.</p>
         <img src="https://source.unsplash.com/random/400x200" alt="" />
-    </div>
+        <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+        <p>
+            What do I like to do? Lots of course but definitely enjoy building
+            websites.
+        </p>
+        <Link to="/contact/">Contact</Link>
+    </Layout>
+  </div>
 )
